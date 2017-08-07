f11a99d3f480ff1034bcd98a2e76ff08a1f1def1
guminli <guminli@baidu.com>
2017-08-07 10:45:57 +0800

    风险分发模块

--- DispatcherConfig.py
+++
@@ -1,8 +0,0 @@
-#encoding: utf-8
-
-DispatchMap = {}
-DispatchMap[u"RISK邮件组"] = ['SingleMail']
-DispatchMap[u'公关部门']   = ['PushToHi']
-DispatchMap[u'商业蓝军'] = ['ApiToLanjun']
-
-WholeProcessorList = ['RecordStat']
--- README.md
+++
@@ -1,2 +0,0 @@
-# xpathanay
-xpath lib for news
